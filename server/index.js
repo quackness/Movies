647b3e7f4c539c29d88fdf8ddb7b141f3d8d591a
Karolina Redden <dubajkaro@gmail.com>
2022-07-15 18:13:47 +0000

    set up server and middleware

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 8001;
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json()); //req.body
+app.use(morgan('dev'));
+
+app.listen(PORT, () => {
+  console.log(`Movies app running on port ${PORT}`);
+})
