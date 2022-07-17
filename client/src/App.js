ff365ba2050cf0e6c0bd993d72711d8624e74104
Karolina Redden <dubajkaro@gmail.com>
2022-07-17 19:45:33 +0000

    add bootstrap import edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import GenresList from './components/GenresList';
+import NewGenre from './components/NewGenre';
+import NewMovie from './components/NewMovie';
+import MoviesList from './components/MoviesList';
 
 function App() {
   return (
     <div className="App">
-      NewMovie
-      NewGenre
-      MoviesList
-      GenresList
+      <div className = "add_movie_and_genre">
+      <NewMovie movies={movies} genres={genres} setMovies={setMovies}/>
+      <NewGenre genres={genres} setGenres={setGenres}/>
+      </div>
+      <MoviesList movies={movies} setMovies={setMovies}/>
+      <GenresList genres={genres} setGenres={setGenres}/>
     </div>
   );
 }
